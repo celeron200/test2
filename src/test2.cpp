54538809956795628d34324a3ee34301e38faa7f
celeron200 <celeron200@163.com>
2016-01-30 22:26:19 +0800

    tttt

---
+++ src/test2.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : test2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=1;
+	int b=2;
+	string c="3";
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	a++;
+	cout <<a<<endl;
+	return 0;
+}
